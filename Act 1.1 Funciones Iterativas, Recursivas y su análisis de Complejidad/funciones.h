7b64566ab928fb7397a451aa69720d896da27956
DanielRFigueroa <dannyrfcr710@gmail.com>
2021-11-04 20:54:30 -0600

    Agrego archivo corregido

---
+++ Act 1.1 Funciones Iterativas, Recursivas y su análisis de Complejidad/funciones.h
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Funciones{
+    public:
+        Funciones();
+        int sumaIterativa(int num);
+        int sumaRecursiva(int num);
+        int sumaDirecta(int num);
+};
+
+Funciones::Funciones(){}
+int Funciones::sumaIterativa(int num){
+    int sumi=0;
+    for (int i = 0; i <= num; i++)
+    {
+        sumi+=i;
+    }
+
+    return sumi;
+}
+int Funciones::sumaRecursiva(int num){
+    if (num==1)
+    {
+        return 1;
+    }
+    else{
+        return(num+sumaRecursiva(num-1));
+    }
+}
+int Funciones::sumaDirecta(int num){
+    return(num*(num+1)/2);
+}
